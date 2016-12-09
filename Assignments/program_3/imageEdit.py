5c0286e57e3c60210b61ff4219033e283e5c5c0f
otrejo-tx <otrejo1200@gmail.com>
2016-12-09 14:45:30 -0600

    Update imageEdit.py

--- Assignments/program_3/imageEdit.py
+++ Assignments/program_3/imageEdit.py
@@ -1,5 +1,7 @@
-import sys
 from PIL import Image
+import os
+import sys
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 import random
 import urllib, cStringIO
 
@@ -145,58 +147,3 @@ class ImageEdit(object):
     	
     	blankimg.save(self.output)
     	return blankimg
-        
-print(sys.argv)
-
-flags = ['-i','-o','-x','-h']
-
-commands = {}
-
-for i in range(1,len(sys.argv),2):
-    commands[sys.argv[i]] = sys.argv[i+1]
-
-commands['-x'] = commands['-x'].split(',')
-print(commands)
-
-if '-i' in commands.keys():
-    input_file = commands['-i']
-    
-if '-o' in commands.keys():
-    output_file = commands['-o']
-else:
-    output_file = (str(commands['-x'])+'.jpg')
-
-if commands['-x'][0] == 'glass_effect':
-	img = Image.open(input_file)
-	ie = ImageEdit(img,output_file)
-	ie.glass_effect()
-	
-elif commands['-x'][0] == 'v_flip':
-	img = Image.open(input_file)
-	ie = ImageEdit(img,output_file)
-	ie.v_flip()
-	
-elif commands['-x'][0] == 'h_flip':
-	img = Image.open(input_file)
-	ie = ImageEdit(img,output_file)
-	ie.h_flip()
-	
-elif commands['-x'][0] == 'blur':
-	img = Image.open(input_file)
-	ie = ImageEdit(img,output_file)
-	ie.blur()
-	
-elif commands['-x'][0] == 'posterize':
-	img = Image.open(input_file)
-	ie = ImageEdit(img,output_file)
-	ie.posterize()
-	
-elif commands['-x'][0] == 'solarize':
-	img = Image.open(input_file)
-	ie = ImageEdit(img,output_file)
-	ie.solarize()
-	
-elif commands['-x'][0] == 'warhol':
-	img = Image.open(input_file)
-	ie = ImageEdit(img,output_file)
-	ie.warhol()
